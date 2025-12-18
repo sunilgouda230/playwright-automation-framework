fd89652fa45c086a9181f6a7e36f6ed690b8724a
Sunil Kumar Gouda <sunilkumargouda@Sunils-MacBook-Pro.local>
2025-12-18 12:51:11 +0530

    removed-test-cases

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -8,16 +8,6 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
 test('test example', async ({ page }) => {
   await page.goto('/');
   console.log('Base URL is:', page.url());
